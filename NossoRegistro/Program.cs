224ce57454677aa3522e377cfd2fdc9076cc74ce
GSCaldas <116302721+GabrielkonCafe@users.noreply.github.com>
2022-10-20 15:58:46 -0400

    Update Program.cs

--- NossoRegistro/Program.cs
+++ NossoRegistro/Program.cs
@@ -1,2 +1,5 @@
 ﻿
 Console.WriteLine("Aqui vão as assinaturas escritas por nós durante a oficina!");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Não desanime, pois até nas profundezas do abismo existe uma luz. ~ CALDEIRA, GABRIEL");
