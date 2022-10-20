086bf9c45b975f46ce6774150cf643d8413c0389
Thiago3101 <116303614+Thiago3101@users.noreply.github.com>
2022-10-20 16:00:06 -0400

    Assinatura
    Assinatura de Thiago Samasquini.

--- NossoRegistro/Program.cs
+++ NossoRegistro/Program.cs
@@ -1,2 +1,3 @@
 ﻿
 Console.WriteLine("Aqui vão as assinaturas escritas por nós durante a oficina!");
+Console.WriteLine("Thiago S. Samasquini - 20/10/2022, (Se alguém te ofendeu sem que vc mereça, volte lá e mereça.)");
