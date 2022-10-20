8cf43437f019c26a5690cf17ee0fd42fa529476a
Nicolas Vailant Dutra  Capila <84550737+nicolascapila@users.noreply.github.com>
2022-10-20 16:02:40 -0400

    coloquei minha assinatura
    :)

--- NossoRegistro/Program.cs
+++ NossoRegistro/Program.cs
@@ -1,2 +1,4 @@
 ﻿
 Console.WriteLine("Aqui vão as assinaturas escritas por nós durante a oficina!");
+
+Console.WriteLine("Nicolas Capila passou por essa oficina, VALEU pela aula Gabriel antunes; 20/10/2022!!");
