7180e9baf9094aa557c2685e258144ef1045c7d5
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2022-10-20 16:44:09 -0400

    mensagem explicativa

--- NossoRegistro/Program.cs
+++ NossoRegistro/Program.cs
@@ -4,3 +4,6 @@ Console.WriteLine("Aqui vão as assinaturas escritas por nós durante a oficina!
 Console.WriteLine("tbm, te amo vanessa");
 Console.WriteLine("t amo rogerio");
 Console.WriteLine("Matheus Silva Samasquini. - 20/10/2022 ('Programar é o minimo que se espera que você  saiba fazer.' - Henrique, Jackson. 2022)");
+
+
+// tal
